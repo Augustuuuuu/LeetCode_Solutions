e2519879b8a3eeffb1cd58a1f9f069f264160fe2
Augusto Saboia <134610628+Augustuuuuu@users.noreply.github.com>
2025-04-11 14:22:10 -0300

    twoSum adicionado
    Fiz duas twoSum, um com o tempo de execução O(n) e outro com execução O(n²)

---
+++ Soluções/Java/twoSum_On.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+class twoSum_On {
+    public int[] twoSum(int[] nums, int target){
+        // Mapa que vai guardar: número -> índice
+        HashMap<Integer, Integer> mapa =  new HashMap<>();
+
+        // Percorrer o array
+        for(int i = 0; i < nums.length; i++){
+            int numeroAtual = nums[i];
+            int complemento = target - numeroAtual;
+
+            // Verificar se o complemento já foi visto
+
+            if(mapa.containsKey(complemento)){
+                // Se sim, retorna o índice do complemento e o índice atual
+                return new int[] { mapa.get(complemento), i};
+
+            }
+            // Se não, guarda o número atual e seu índice.
+
+            mapa.put(numeroAtual, i);
+
+        }
+        return new int[] {};
+    }
+
+}
